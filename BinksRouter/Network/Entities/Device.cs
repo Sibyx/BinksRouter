8070282655b4e90111a6c7962ceda72ab472c5b2
Jakub Dubec <jakub.dubec@gmail.com>
2020-03-01 13:50:54 +0100

    Initial 😼

---
+++ BinksRouter/Network/Entities/Device.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Net;
+using System.Runtime.CompilerServices;
+using BinksRouter.Annotations;
+using SharpPcap.Npcap;
+
+namespace BinksRouter.Network.Entities
+{
+    class Device : INotifyPropertyChanged
+    {
+        private bool _isOpened;
+        private string _name;
+        private IPAddress _ipAddress;
+        private readonly NpcapDevice _captureDevice;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
